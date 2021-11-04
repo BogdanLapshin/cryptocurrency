749a1cc8374bd691f5aa8964f9100fa064ec31bc
BogdanLapshin <bogdanizizmaila@gmail.com>
2021-11-04 09:49:33 +0200

    complete footer block

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,16 @@
-import './Footer.module.scss';
+import styles from './Footer.module.scss';
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.comment}>Don’t Forget To Deploy Project To GitHub Pages</div>
+        <div className={styles.copyright}>
+          <div className={styles.info}>© Test Task Limited™, 2020. All rights reserved.</div>
+          <div className={styles.terms}>Terms Of Service | Privacy Policy</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
